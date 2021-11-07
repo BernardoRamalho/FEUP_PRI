8333d70a17549d73c5d9d389dcd61507503176df
supertommc <correiodotomas@gmail.com>
2021-11-07 19:05:16 +0000

    Added datasets as zip and finished data cleaning and datasets splitting

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,9 +5,13 @@ df = pd.read_csv('GoodReads_100k_books.csv')
 
 # Drop the isbn13 column, as it is not useful
 df = df.drop(['isbn13'], axis=1)
+print("# Column 'isbn13' dropped.")
+print()
 
 # Drop the row corresponding to a book that has no title
 df = df.drop(df[df['title'].isnull()].index)
+print("# Dropped rows that have no title.")
+print()
 
 # Replace missing values
 df['bookformat'] = df['bookformat'].fillna("Unknown")
@@ -15,8 +19,28 @@ df['desc'] = df['desc'].fillna("No description available")
 df['genre'] = df['genre'].fillna("Unknown")
 df['img'] = df['img'].fillna("Not available")
 df['isbn'] = df['isbn'].fillna("Not available")
+print("# Replaced the missing values on columns 'bookformat', 'desc', 'genre', 'img' and 'isbn'.")
+print()
 
+# Remove duplicate genres and authors
+for index, row in df.iterrows():
+    genres = str(row['genre']).split(',')
+    authors = str(row['author']).split(',')
+    no_dups_genres = list(set(genres))
+    no_dups_authors = list(set(authors))
+    df.at[index, 'genre'] = ','.join(no_dups_genres)
+    df.at[index, 'author'] = ','.join(no_dups_authors)
+
+print("# Removed duplicate genres and authors.")
+print()
+
+# Give the dataset an index that will also serve as each book's id
+df = df.reset_index()
+df = df.drop('index', axis=1)
 df.index.name = 'book_id'
+print("# Added the 'book_id' column.")
+print()
 
 # Save the cleaned dataset on a new csv file
 df.to_csv('dataset.csv')
+print("# Saved the dataset as 'dataset.csv'.")
--- split_dataset.py
+++ split_dataset.py
@@ -5,55 +5,127 @@ clean_df = pd.read_csv('dataset.csv')
 
 if not os.path.exists('genres.csv'):
 
+    print("# Generating file 'genres.csv'.")
+
     # Generate the genres dataset
     genres_df = clean_df[['book_id', 'genre']].copy()
-    ids_list = []
-    genres_list = []
     genres_dict = {}
 
     for index, row in genres_df.iterrows():
         genres = str(row['genre']).split(',')
-        book_id = int(row['book_id'])
+        book_id = str(row['book_id'])
         for genre in genres:
-            '''
             if genre not in genres_dict:
                 genres_dict[genre] = [book_id]
             else:
                 genres_dict[genre].append(book_id)
-            '''
-            ids_list.append(book_id)
-            genres_list.append(genre)
-
-    assert(len(ids_list) == len(genres_list))
+            
+    genres_list = []
+    for genre_list in [*genres_dict.values()]:
+        genres_list.append(','.join(genre_list))
 
     final_genres_df = pd.DataFrame({
-        'book_id': ids_list,
+        'book_id': [*genres_dict.keys()],
         'genre': genres_list
         })
 
-    '''
-    assert(len(genres_dict.keys()) == len(genres_dict.values()))
+    print("## Sorting the dataframe.")
+    final_genres_df = final_genres_df.sort_values(by=['genre'])
 
-    alt_genres_df = pd.DataFrame({
-        'genre': genres_dict.keys(),
-        'ids': genres_dict.values()
-    })
-    '''
+    print("# File 'genres.csv' generated successfully.")
+    print()
 
     # Save the genres dataset
     final_genres_df.to_csv('genres.csv', index=False)
-    #alt_genres_df.to_csv('alt_genres.csv', index=False)
+else:
+    print("# File 'genres.csv' already exists, skipping generation for this file.")
+    print()
 
 if not os.path.exists('ratings.csv'):
-
     # Generate the ratings dataset
+    print("# Generating file 'ratings.csv'.")
+
     ratings_df = clean_df[['book_id', 'rating', 'totalratings']].copy()
 
     ratings_df.to_csv('ratings.csv', index=False)
 
-if not os.path.exists('authors.csv'):
+    print("# File 'ratings.csv' generated successfully.")
+    print()
+else:
+    print("# File 'ratings.csv' already exists, skipping generation for this file.")
+    print()
 
+if not os.path.exists('authors.csv'):
     # Generate the authors dataset
 
-    # Not implemented yet
-    pass
+    print("# Generating file 'authors.csv'.")
+    
+    authors_dict = {}
+
+    authors_df = clean_df[['book_id', 'author']].copy()
+
+    i = 0
+    for index, row in authors_df.iterrows():
+        authors = str(row['author']).split(',')
+        book_id = str(row['book_id'])
+
+        for author in authors:
+            if author not in authors_dict:
+                authors_dict[author] = [book_id]
+            else:
+                authors_dict[author].append(book_id)
+
+    authors_list = []
+    for author_list in [*authors_dict.values()]:
+        authors_list.append(','.join(author_list))
+
+    intermediate_authors_df = pd.DataFrame({
+        'author': [*authors_dict.keys()],
+        'ids': authors_list
+        })
+
+    print("## Calculating the average rating for each author.")
+
+    averages = []
+
+    for index, row in intermediate_authors_df.iterrows():
+        ids = list(map(lambda x: int(x), str(row['ids']).split(',')))
+        average = 0.0
+        for id in ids:
+            try:
+                average += float(clean_df.iloc[id]['rating'])
+            except:
+                print(id)
+
+        averages.append(average/len(ids))
+    
+    final_authors_df = pd.DataFrame({
+        'author': [*authors_dict.keys()],
+        'ids': authors_list,
+        'average_rating': averages
+    })
+
+    print("## Sorting the dataframe.")
+    final_authors_df = final_authors_df.sort_values(by=['author'])
+
+    final_authors_df.to_csv('authors.csv', index=False)
+
+    print("# File 'authors.csv' generated successfully.")
+    print()
+else:
+    print("# File 'authors.csv' already exists, skipping generation for this file.")
+    print()
+
+if not os.path.exists('books.csv'):
+    # Generate the books dataset
+    print("# Generating file 'books.csv'.")
+
+    clean_df = clean_df.drop(['genre', 'author', 'rating', 'totalratings'], axis=1)
+
+    clean_df.to_csv('books.csv', index=False)
+
+    print("# File 'books.csv' generated successfully.")
+    print()
+else:
+    print("# File 'books.csv' already exists, skipping generation for this file.")
+    print()
