36bc25abd72a4adb0027650a1ca361fadcf2c897
supertommc <correiodotomas@gmail.com>
2021-12-08 12:40:13 +0000

    System1

--- solr_one_doc/system1/startup.sh
+++ solr_one_doc/system1/startup.sh
@@ -7,6 +7,13 @@ solr start
 
 sleep 10
 
+# Schema definition via API
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary @/data/data_schema.json \
+    http://localhost:8983/solr/books/schema
+
+sleep 1
+
 # Populate collection
 bin/post -c books /data/data.json
 
