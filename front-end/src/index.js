7066abe0c472ed4b0aa1c00db37497a395a5c478
Leonor Marques Gomes <up201806567@fe.up.pt>
2022-01-12 11:43:49 +0000

    Atempt to connect

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,18 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios';
+
 
 
 class SearchApp extends React.Component {
 
+    constructor(props){
+      super(props);
+      this.data = null;
+      
+    }
+
+    getQuery(){
+      console.log("QUERYRYRYRYRYYRYR")
+      axios('http://localhost:8983/solr/books/select?q=fiction%20paperback&q.op=OR&defType=edismax&indent=true&rows=100&qf=author%5E1%20desc%5E1%20title%5E1%20genre%5E1%20bookformat%5E1')
+      .then( response => {this.setData(response.data)})
+    }
+
+    setData(data){
+      console.log(data)
+      this.data = data
+      console.log(this.data)
+    }
 
     render() {
+        this.getQuery();
+        
 
-        fetch('http://localhost:8983/solr/books/select?q=fiction%20paperback&q.op=OR&defType=edismax&indent=true&rows=100&qf=author%5E1%20desc%5E1%20title%5E1%20genre%5E1%20bookformat%5E1')
-        .then(response => console.log(response.json()))
+        console.log(this.data)
+
         return(<div>
-            "adfasdf"
 
-        </div>)
+            <div>
+              {this.data.docs[0]}
+            </div>
+            
+
+        </div>
+        )
     }
   }
   
