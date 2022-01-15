de5ba41a1f392c83e749339f722957f1ac654f31
supertommc <correiodotomas@gmail.com>
2022-01-15 11:40:05 +0000

    Algo

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,11 +4,14 @@ import ReactDOM from 'react-dom'
 
 class SearchApp extends React.Component {
 
-
     render() {
 
-        fetch('http://localhost:8983/solr/books/select?q=fiction%20paperback&q.op=OR&defType=edismax&indent=true&rows=100&qf=author%5E1%20desc%5E1%20title%5E1%20genre%5E1%20bookformat%5E1')
-        .then(response => console.log(response.json()))
+        let user_query = "job";
+        let search_fields = encodeURIComponent("author desc title genre bookformat review")
+
+        fetch(`http://localhost:8983/solr/books/select?q=${user_query}%20OR%20%7B!parent%20which%3D%22type:book%22%7D%7B!edismax%20qf%3D%22review%22%20v%3D%22${user_query}%22%7D&q.op=OR&defType=edismax&indent=true&qf=${search_fields}&fl=*,%20%5Bchild%5D&fq=!type:review&rows=100`)
+        .then(response => response.text())
+        .then((response) => console.log(JSON.parse(response)['response']['docs']))
         return(<div>
             "adfasdf"
 
