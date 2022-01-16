48fd9d3f0fd02357be941414cdbc9f4fe211d0b5
Leonor Marques Gomes <up201806567@fe.up.pt>
2022-01-16 15:12:09 +0000

    Front end started

--- front-end-2/app.py
+++ front-end-2/app.py
@@ -12,4 +12,13 @@ def home():
     print(results)
     return render_template('index.html', results=results)
 
+"""
+@app.route("/showResults")
+def home():
+    query_url = "http://localhost:8983/solr/books/select?q=*:*&fq=!type:review&q.op=OR&defType=lucene&indent=true&rows=10"
+    results = requests.get(query_url).json()['response']['docs']
+    print(results)
+    return render_template('results.html', results=results)
+"""
+
 app.run(debug = True)
